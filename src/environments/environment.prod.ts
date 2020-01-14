d044831339ebe738b9ea79ce352f6effe79f8047
dev-maven <omoakin1fe@gmail.com>
2020-01-14 15:54:34 +0100

    Theme

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyAfm07LoZ_OILdvSD3nO5weNCfMWq317oY',
+    authDomain: 'fitness-tracker-a31e1.firebaseapp.com',
+    databaseURL: 'https://fitness-tracker-a31e1.firebaseio.com',
+    projectId: 'fitness-tracker-a31e1',
+    storageBucket: 'fitness-tracker-a31e1.appspot.com',
+    messagingSenderId: '736957626165',
+    appId: '1:736957626165:web:6a4cac4c2074a25994fe7c',
+    measurementId: 'G-PYSK0T5KE6'
+
+  }
 };
